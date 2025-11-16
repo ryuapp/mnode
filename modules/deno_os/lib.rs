4b28891e3f1995af39f64ac1060122db4dc31502
ryu <114303361+ryuapp@users.noreply.github.com>
2025-11-17 01:09:51 +0900

    feat: add Deno.env

--- modules/deno_os/lib.rs
+++ modules/deno_os/lib.rs
@@ -1,5 +1,7 @@
 // Copyright 2018-2025 the Deno authors. MIT license.
 use rquickjs::Ctx;
+use std::collections::HashMap;
+use std::env;
 use utils::add_internal_function;
 
 pub fn init(ctx: &Ctx<'_>) -> rquickjs::Result<()> {
@@ -8,10 +10,35 @@ pub fn init(ctx: &Ctx<'_>) -> rquickjs::Result<()> {
 }
 
 fn setup_internal(ctx: &Ctx) -> Result<(), Box<dyn std::error::Error>> {
+    // Deno.exit
     add_internal_function!(ctx, "exit", |code: Option<i32>| -> i32 {
         let exit_code = code.unwrap_or(0);
         std::process::exit(exit_code);
     });
+
+    // Deno.env
+    {
+        ctx.eval::<(), _>("globalThis[Symbol.for('mdeno.internal')].env = {};")?;
+        add_internal_function!(ctx, "env.get", |key: String| -> Option<String> {
+            env::var(&key).ok()
+        });
+        add_internal_function!(ctx, "env.set", |key: String, value: String| {
+            unsafe {
+                env::set_var(&key, value);
+            }
+        });
+        add_internal_function!(ctx, "env.delete", |key: String| {
+            unsafe {
+                env::remove_var(&key);
+            }
+        });
+        add_internal_function!(ctx, "env.has", |key: String| -> bool {
+            env::var(&key).is_ok()
+        });
+        add_internal_function!(ctx, "env.toObject", || -> HashMap<String, String> {
+            env::vars().collect()
+        });
+    }
 
     Ok(())
 }
