c229cb1db7cce74afbccbf2b2386ce2d21c48034
ryu <114303361+ryuapp@users.noreply.github.com>
2025-11-17 23:45:53 +0900

    feat: add Deno.env (#24)

--- modules/deno_os/deno_os.js
+++ modules/deno_os/deno_os.js
@@ -3,6 +3,24 @@ globalThis.Deno ||= {};
 
 // https://docs.deno.com/api/deno/~/Deno.exit
 globalThis.Deno.exit = function (code) {
-  const internal = globalThis[Symbol.for("mdeno.internal")];
   internal.exit(code);
+};
+
+// https://docs.deno.com/api/deno/~/Deno.Env
+globalThis.Deno.env = {
+  get: function (key) {
+    return internal.env.get(key);
+  },
+  set: function (key, value) {
+    internal.env.set(key, value);
+  },
+  delete: function (key) {
+    internal.env.delete(key);
+  },
+  has: function (key) {
+    return internal.env.has(key);
+  },
+  toObject: function () {
+    return internal.env.toObject();
+  },
 };
